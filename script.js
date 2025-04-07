8e14ba3cf952ec73c92b1da911616560210c5dd9
Adil Oryspayev <38333869+adiloryspayev@users.noreply.github.com>
2025-04-07 19:21:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+const gridContainer = document.getElementById('grid');
+
+if (gridContainer) {
+    for (let i = 0; i < 9; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.dataset.index = i;
+        gridContainer.appendChild(cell);
+    }
+}
+
+let guesses = 0;
+document.getElementById("guesses").textContent = guesses;
+
+const cells = document.querySelectorAll(".cell");
+const playerInput = document.getElementById("playerInput");
+
+const cols = ["cat", "cat2", "cat3"];
+const rows = ["cat4", "cat5", "cat6"];
+
+
+if (document.getElementById("c0")) {
+    document.getElementById("c0").textContent = cols[0];
+}
+if (document.getElementById("c1")) {
+    document.getElementById("c1").textContent = cols[1];
+}
+if (document.getElementById("c2")) {
+    document.getElementById("c2").textContent = cols[2];
+}
+if (document.getElementById("r0")) {
+    document.getElementById("r0").textContent = rows[0];
+}
+if (document.getElementById("r1")) {
+    document.getElementById("r1").textContent = rows[1];
+}
+if (document.getElementById("r2")) {
+    document.getElementById("r2").textContent = rows[2];
+}
+
+cells.forEach(cell => {
+    cell.addEventListener("click", function () {
+        playerInput.focus();
+        playerInput.dataset.selectedCell = this.dataset.index;
+    });
+});
+
+if (playerInput) {
+    playerInput.addEventListener("keypress", function (event) {
+        if (event.key === "Enter") {
+            guesses++;
+            document.getElementById("guesses").textContent = guesses;
+            const selectedCellIndex = this.dataset.selectedCell;
+            if (selectedCellIndex !== undefined) {
+                const selectedCategories = [];
+                let index = parseInt(selectedCellIndex);
+
+                // this.value is the guess -> check that
+
+                // check guess correct in both of categories:
+                // cols[index%3]
+                // rows[(index-(index%3))/3]
+
+                // if correct, push player name/picture onto cell
+                // selectedCategories.push(____)
+                selectedCategories.push(this.value);
+                selectedCategories.push(cols[index%3]);
+                selectedCategories.push(rows[(index-(index%3))/3]);
+
+                cells[selectedCellIndex].textContent = selectedCategories.join(", ");
+                this.value = ""; // Clear input field
+            }
+        }
+    });
+}
+
+
+
+
+
+
